132017c82618d75c6f762b70ef32c3f15a47e752
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-11 18:25:53 +0530

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Rishi Singh on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
