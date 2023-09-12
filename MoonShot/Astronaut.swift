791a96ec3616660f9ee6658aff0d61d78ddc350b
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-12 19:56:54 +0530

    Loading a specific kind of Codable data

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Rishi Singh on 12/09/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -8,17 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    let layout = [
-        GridItem(.adaptive(minimum: 80))
-    ]
+    let astronauts = Bundle.main.decode("astronauts.json")
+
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: layout) {
-                ForEach(0..<1000) {
-                    Text("Item \($0)")
-                }
-            }
-        }
+        Text("\(astronauts.count)")
     }
 }
 
